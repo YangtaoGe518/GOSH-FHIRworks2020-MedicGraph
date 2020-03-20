39906b6f3bbc6103b6289ac6d708b3f2a4915414
Yangtao Ge <geyangtao@yahoo.com>
2020-03-20 14:53:31 +0000

    init

---
+++ src/test/java/uk/ac/ucl/medicgraph/MedicGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.ac.ucl.medicgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
