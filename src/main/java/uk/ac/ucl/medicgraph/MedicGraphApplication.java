3e588076f129a886f5953d872425ce6464876f27
Yangtao Ge <geyangtao@yahoo.com>
2020-03-20 23:37:06 +0000

    add cros config

--- src/main/java/uk/ac/ucl/medicgraph/MedicGraphApplication.java
+++ src/main/java/uk/ac/ucl/medicgraph/MedicGraphApplication.java
@@ -4,8 +4,8 @@ import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @EnableSwagger2Doc
+@SpringBootApplication
 public class MedicGraphApplication {
 
 	public static void main(String[] args) {
