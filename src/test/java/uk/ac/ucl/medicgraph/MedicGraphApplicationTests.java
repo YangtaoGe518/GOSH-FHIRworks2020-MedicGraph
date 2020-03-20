5927b8a572698c8d3eece57c8e73c43f8e0e65b1
Yangtao Ge <geyangtao@yahoo.com>
2020-03-20 20:13:56 +0000

    add new global properties

--- src/test/java/uk/ac/ucl/medicgraph/MedicGraphApplicationTests.java
+++ src/test/java/uk/ac/ucl/medicgraph/MedicGraphApplicationTests.java
@@ -1,13 +1,24 @@
 package uk.ac.ucl.medicgraph;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class MedicGraphApplicationTests {
+	@Value("${uk.ac.ucl.medic-graph.datasource}")
+	private String dataUrl;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testGlobalProps(){
+		assertEquals("localhost:5001", dataUrl);
 	}
 
 }
