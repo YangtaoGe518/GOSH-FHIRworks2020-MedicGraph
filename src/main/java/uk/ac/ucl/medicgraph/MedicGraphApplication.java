898b1eed0eb51020712c1f5170c82c4b33f7ce8d
Yangtao Ge <geyangtao@yahoo.com>
2020-03-20 23:35:32 +0000

    add swagger

--- src/main/java/uk/ac/ucl/medicgraph/MedicGraphApplication.java
+++ src/main/java/uk/ac/ucl/medicgraph/MedicGraphApplication.java
@@ -1,9 +1,11 @@
 package uk.ac.ucl.medicgraph;
 
+import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSwagger2Doc
 public class MedicGraphApplication {
 
 	public static void main(String[] args) {
