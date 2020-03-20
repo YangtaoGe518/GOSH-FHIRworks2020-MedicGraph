51ea4e76df7d575e99dfb1d1550c5c74aa5af7fa
Yangtao Ge <geyangtao@yahoo.com>
2020-03-20 23:35:27 +0000

    add hello test

---
+++ src/main/java/uk/ac/ucl/medicgraph/controller/HelloWordController.java
@@ -0,0 +1,22 @@
+package uk.ac.ucl.medicgraph.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import uk.ac.ucl.medicgraph.domain.Hello;
+
+@RestController
+@RequestMapping(value = "/hello")
+@Api(value ="Hello World Controller", tags={"Hello World Controller"})
+public class HelloWordController {
+
+    @GetMapping("/{name}")
+    @ApiOperation(value = "Test path variable api document")
+    public Hello sayHelloTo(@PathVariable("name") String name){
+        Hello hello = new Hello(name, ", Nice to meet you");
+        return hello;
+    }
+}
---
+++ src/main/java/uk/ac/ucl/medicgraph/domain/Hello.java
@@ -0,0 +1,15 @@
+package uk.ac.ucl.medicgraph.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hello {
+    private String name;
+    private String greetings;
+
+}
