e4225d4473fd507dd630c7b977a75d0da19e1b27
Yangtao Ge <geyangtao@yahoo.com>
2020-03-22 01:39:51 +0000

    add multiple list test

--- src/test/java/uk/ac/ucl/medicgraph/patientTest/ObservationTest.java
+++ src/test/java/uk/ac/ucl/medicgraph/patientTest/ObservationTest.java
@@ -1,13 +1,19 @@
 package uk.ac.ucl.medicgraph.patientTest;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import uk.ac.ucl.medicgraph.config.DataSourceConfig;
+import uk.ac.ucl.medicgraph.domain.request.listView.ListView;
 import uk.ac.ucl.medicgraph.domain.request.observation.Observation;
 import uk.ac.ucl.medicgraph.domain.request.observation.Subject;
+import uk.ac.ucl.medicgraph.domain.request.patient.Patient;
 import uk.ac.ucl.medicgraph.util.HttpRequest;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 @SpringBootTest
 public class ObservationTest {
@@ -26,6 +32,23 @@ public class ObservationTest {
 
         System.out.println(observation);
 
+    }
+
+    @Test
+    public void testDeserializeMulti(){
+        String url = dataSourceConfig.getDataUrl() + "/api/Observation/aa0c4f0f-6a93-4f65-a9a9-2faf189d24d9";
+
+        Type observationListType = new TypeToken<List<ListView<Observation>>>(){}.getType();
+
+        String json = HttpRequest.requestJson(url);
+//        System.out.println(json);
+
+
+        Gson gson = new Gson();
+
+        List<ListView<Observation>> observationListView = gson.fromJson(json, observationListType);
+
+        System.out.println(observationListView);
     }
 
 
